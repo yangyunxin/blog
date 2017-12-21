d51ca2eea5b3fee7d92fc84734cbda98a77dff5e
杨运鑫 <850801584@qq.com>
2017-12-21 10:15:10 +0800

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-gulp.task('sass', () => {
-	return gulp.src('src/scss/*.scss')
-	.pipe(sass().on('error', sass.logError))
-	.pipe(cleanCSS({compatibility: 'ie8'}))
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest('server/public/css'))
-})
